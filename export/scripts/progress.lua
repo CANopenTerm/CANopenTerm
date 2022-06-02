0d6cf10d186aa594c954686ddfcc4edc3660ea67
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-06-02 09:35:12 +0200

    Add progress display example script

---
+++ export/scripts/progress.lua
@@ -0,0 +1,12 @@
+progress = 0
+for i = 1, 100, 1
+do
+    progress = progress + 1
+
+    io.write("\rProgress: ")
+    io.write(progress)
+    io.write("%")
+    delay_ms(100)
+end
+io.write('\n')
+io.flush()
