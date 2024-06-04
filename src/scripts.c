328f7a27e5fc5d8ee743bd1c934404fe723defab
Michael Fitzmayer <mfitzmayer@jetter.de>
2024-06-04 11:24:38 +0200

    Add additional script search paths on Linux, closes #9

--- src/scripts.c
+++ src/scripts.c
@@ -11,6 +11,15 @@
 #include "conio.h"
 #endif
 
+#ifdef USE_LIBSOCKETCAN
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+
+static const char* script_dirs[] = { "/usr/local/share/CANopenTerm/scripts", "/usr/share/CANopenTerm/scripts", "./scripts" };
+#endif
+
 #include "SDL.h"
 #include "lua.h"
 #include "lualib.h"
@@ -56,31 +65,79 @@ void scripts_deinit(core_t* core)
 
 void list_scripts(void)
 {
-    DIR *dir;
+    DIR* dir;
 
-    dir = opendir("./scripts");
-    if (NULL != dir)
+#ifdef USE_LIBSOCKETCAN
+    int i;
+    for (i = 0; i < sizeof(script_dirs) / sizeof(script_dirs[0]); i++)
     {
-        struct dirent *ent;
-        while (NULL != (ent = readdir (dir)))
+        dir = opendir(script_dirs[i]);
+        if (NULL != dir)
         {
-            switch (ent->d_type)
+            struct dirent* ent;
+            while (NULL != (ent = readdir(dir)))
             {
+                switch (ent->d_type)
+                {
                 case DT_REG:
-                    puts(ent->d_name);
+                    printf("%s/%s\n", script_dirs[i], ent->d_name);
                     break;
                 case DT_DIR:
                 case DT_LNK:
                 default:
                     break;
+                }
+            }
+            closedir(dir);
+        }
+    }
+#else
+    dir = opendir("./scripts");
+    if (NULL != dir)
+    {
+        struct dirent* ent;
+        while (NULL != (ent = readdir(dir)))
+        {
+            switch (ent->d_type)
+            {
+            case DT_REG:
+                puts(ent->d_name);
+                break;
+            case DT_DIR:
+            case DT_LNK:
+            default:
+                break;
             }
         }
-        closedir (dir);
+        closedir(dir);
     }
+#endif
 }
 
 void run_script(const char* name, core_t* core)
 {
+#ifdef USE_LIBSOCKETCAN
+    char script_path[256] = { 0 };
+    const char* script_dirs[] = { "/usr/local/share/CANopenTerm/scripts", "/usr/share/CANopenTerm/scripts", "./scripts" };
+    int i;
+
+    if (NULL == core)
+    {
+        return;
+    }
+
+    for (i = 0; i < sizeof(script_dirs) / sizeof(script_dirs[0]); i++)
+    {
+        snprintf(script_path, sizeof(script_path), "%s/%s", script_dirs[i], name);
+        if (LUA_OK == luaL_dofile(core->L, script_path))
+        {
+            lua_pop(core->L, lua_gettop(core->L));
+            return;
+        }
+    }
+
+    c_log(LOG_WARNING, "Could not run script '%s'", name);
+#else
     char script_path[64] = { 0 };
 
     if (NULL == core)
@@ -97,9 +154,10 @@ void run_script(const char* name, core_t* core)
     {
         c_log(LOG_WARNING, "Could not run script '%s': %s", name, lua_tostring(core->L, -1));
     }
+#endif
 }
 
-int lua_delay_ms(lua_State* L)
+int lua_delay_ms(lua_State * L)
 {
     Uint32 delay_in_ms = (Uint32)luaL_checkinteger(L, 1);
 
@@ -107,7 +165,7 @@ int lua_delay_ms(lua_State* L)
     return 1;
 }
 
-int lua_poll_keys(lua_State* L)
+int lua_poll_keys(lua_State * L)
 {
 #ifdef _WIN32
     if (0 != kbhit())
