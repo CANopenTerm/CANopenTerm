8bf2214545ffc71c4fe6e8e395804ffcc7fa43c0
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-06-13 08:58:50 +0200

    Update device scanner script, group by device

--- export/scripts/device_scanner.lua
+++ export/scripts/device_scanner.lua
@@ -10,11 +10,16 @@ local canopen = require "canopen"
 
 local available_nodes, total_devices = canopen.find_devices(1000)
 
-print_heading(string.format("Found %d device(s):", total_devices))
+print(string.format("\nFound %d device(s)", total_devices))
 
 for _, node_id in ipairs(available_nodes) do
+  local device_name = sdo_read(node_id, 0x1008, 0x00)
+  if nil == device_name then
+    device_name = "Unknown device"
+  end
+
+  print_heading(device_name)
   sdo_read(node_id, 0x1000, 0x00, true)
-  sdo_read(node_id, 0x1008, 0x00, true)
   sdo_read(node_id, 0x1009, 0x00, true)
   sdo_read(node_id, 0x100A, 0x00, true)
   sdo_read(node_id, 0x1018, 0x01, true)
@@ -22,3 +27,5 @@ for _, node_id in ipairs(available_nodes) do
   sdo_read(node_id, 0x1018, 0x03, true)
   sdo_read(node_id, 0x1018, 0x04, true)  
 end
+
+print("")
