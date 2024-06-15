f46ecd47bf5a5f61fe57590e14998a7eb7f658e0
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-06-15 11:36:58 +0200

    Add CAN trace script

---
+++ export/scripts/trace.lua
@@ -0,0 +1,28 @@
+﻿--[[
+
+Title:   CAN trace
+Author:  Michael Fitzmayer
+License: Public domain
+
+--]]
+
+
+local start_time_ms = os.time() * 1000 + os.clock() * 1000
+
+print("Time    CAN-ID  Length  Data")
+
+while not key_is_hit() do
+  local id, length, data = can_read()
+
+  if data then
+    local current_time_ms = (os.time() * 1000 + os.clock() * 1000) - start_time_ms
+
+    io.write(string.format("%06d  %03X     %1d       ", current_time_ms, id, length))
+
+    for i = 1, length do
+      io.write(string.format("%02X ", data:byte(i)))
+    end
+
+    io.write("\n")
+  end
+end
