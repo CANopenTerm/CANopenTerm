f8b5071b515cc9059584fea76036a1a1746be212
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-06-19 08:34:19 +0200

    Update can trace player; it should work now

--- export/scripts/replay.lua
+++ export/scripts/replay.lua
@@ -36,6 +36,22 @@ function choose_file(files)
     end
 end
 
+local function convert_data_bytes(data_bytes)
+    local bytes = {}
+    for byte in data_bytes:gmatch("%S+") do
+        table.insert(bytes, byte)
+    end
+    
+    while #bytes < 8 do
+        table.insert(bytes, "00")
+    end
+    
+    local data_d0_d3 = "0x" .. table.concat(bytes, "", 1, 4)
+    local data_d4_d7 = "0x" .. table.concat(bytes, "", 5, 8)
+    
+    return data_d0_d3, data_d4_d7
+end
+
 function format_float(num)
     local formatted_str = string.format("%.1f", num)
     local length_needed = string.len("1234.5")
@@ -103,13 +119,26 @@ else
         print("Chosen file: " .. base_name)
 
         local trc_data = parse_pcan_trc(base_name)
+        local start_time = os.clock()
+
         for _, message in ipairs(trc_data) do
+            if key_is_hit() then
+              break
+            end
+
             if message and message.time_offset and message.msg_type and message.can_id and message.dlc and message.data_bytes then
+                local current_time           = os.clock()
+                local elapsed_time           = (current_time - start_time) * 1000
+                local delay                  = math.floor(message.time_offset - elapsed_time)
+                local data_d0_d3, data_d4_d7 = convert_data_bytes(message.data_bytes)
 
-                -- TBD.
+                if delay > 0 then
+                    delay_ms(delay)
+                end
 
+                can_write(message.can_id, message.dlc, data_d0_d3, data_d4_d7)
                 print(string.format("Time Offset: %s, Msg Type: %s, CAN ID: %s, DLC: %d, Data Bytes: %s",
-                    format_float(message.time_offset), message.msg_type, message.can_id, message.dlc, message.data_bytes))         
+                    format_float(message.time_offset), message.msg_type, message.can_id, message.dlc, message.data_bytes))
             else
                 print("Invalid message format or nil value detected.")
                 print("message: ", message)
