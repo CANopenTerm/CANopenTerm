2f39bdbe9e470a75af867484b52fc6033f080b8a
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-07-15 16:14:00 +0200

    Add missing function in helper library

--- export/lua/canopen.lua
+++ export/lua/canopen.lua
@@ -6,6 +6,34 @@ License: Public domain
 
 --]]
 
+local function find_devices(timeout_ms)
+  local start_time = os.time()
+  local timeout    = timeout_ms / 1000  -- convert milliseconds to seconds
+  local node_ids   = {}
+
+  while os.time() - start_time < timeout do
+      local id, length, message = can_read()
+
+      if id then
+          if id >= 0x701 and id <= 0x77F then
+              local node_id = id - 0x700
+              if not node_ids[node_id] then
+                  node_ids[node_id] = true
+              end
+          end
+      end
+  end
+
+  local node_list = {}
+  for k in pairs(node_ids) do
+      table.insert(node_list, k)
+  end
+
+  local total_devices = #node_list
+
+  return node_list, total_devices
+end
+
 local function get_id_by_name(name)
   local available_nodes, total_devices = find_devices(2000)
 
