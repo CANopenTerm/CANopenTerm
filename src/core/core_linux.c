d3cc77b77a3d88ce5d72ed047e4a61cc8796a138
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-11-17 13:02:22 +0100

    Add missing files, #66

---
+++ src/core/core_linux.c
@@ -0,0 +1,31 @@
+/** @file core_linux.c
+ *
+ *  A versatile software tool to analyse and configure CANopen devices.
+ *
+ *  Copyright (c) 2024, Michael Fitzmayer. All rights reserved.
+ *  SPDX-License-Identifier: MIT
+ *
+ **/
+
+#include <stdlib.h>
+#include <signal.h>
+#include "core.h"
+#include "os.h"
+
+void handle_sigint(int sig)
+{
+    extern core_t* core;
+
+    (void)sig;
+    os_log(LOG_INFO, "Ctrl+C pressed. Cleaning up...");
+    if (core != NULL)
+    {
+        core_deinit(core);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+void core_register_ctrl_c_handler(void)
+{
+    signal(SIGINT, handle_sigint);
+}
---
+++ src/core/core_windows.c
@@ -0,0 +1,34 @@
+/** @file core_windows.c
+ *
+ *  A versatile software tool to analyse and configure CANopen devices.
+ *
+ *  Copyright (c) 2024, Michael Fitzmayer. All rights reserved.
+ *  SPDX-License-Identifier: MIT
+ *
+ **/
+
+#include <windows.h>
+#include "core.h"
+#include "os.h"
+
+BOOL WINAPI handle_ctrl_c(DWORD signal)
+{
+    extern core_t* core;
+
+    if (signal == CTRL_C_EVENT)
+    {
+        if (core != NULL)
+        {
+            os_log(LOG_INFO, "Ctrl+C pressed. Cleaning up...");
+            fflush(stdout);
+            core_deinit(core);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    return TRUE;
+}
+
+void core_register_ctrl_c_handler(void)
+{
+    SetConsoleCtrlHandler(handle_ctrl_c, TRUE);
+}
