b660c7adaef5e9ffc79fb22f59e1fa262a94224a
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-11-19 20:06:43 +0100

    Add Findinih.cmake, #68

---
+++ cmake/Findinih.cmake
@@ -0,0 +1,15 @@
+# Findinih.cmake
+# Locate inih library and include directories
+
+find_path(inih_INCLUDE_DIR NAMES ini.h)
+find_library(inih_LIBRARY NAMES inih)
+
+if (inih_INCLUDE_DIR AND inih_LIBRARY)
+    set(inih_FOUND TRUE)
+    set(inih_INCLUDE_DIRS ${inih_INCLUDE_DIR})
+    set(inih_LIBRARIES ${inih_LIBRARY})
+else()
+    set(inih_FOUND FALSE)
+endif()
+
+mark_as_advanced(inih_INCLUDE_DIR inih_LIBRARY)
