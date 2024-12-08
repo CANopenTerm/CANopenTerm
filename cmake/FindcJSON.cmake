233ac1e59f709629d7e5244839626702cbf23360
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-12-08 13:14:46 +0100

    Fix casing

--- cmake/FindcJSON.cmake
+++ cmake/FindcJSON.cmake
@@ -1,12 +1,10 @@
 # FindcJSON.cmake
 # Locate cJSON library and include directories
 
-find_path(CJSON_INCLUDE_DIR NAMES cJSON.h HINTS /usr/include/cjson)
-message(STATUS "CJSON_INCLUDE_DIR: ${CJSON_INCLUDE_DIR}")
-find_library(CJSON_LIBRARY NAMES cjson HINTS /usr/lib)
-message(STATUS "CJSON_LIBRARY: ${CJSON_LIBRARY}")
+find_path(cJSON_INCLUDE_DIR NAMES cJSON.h HINTS /usr/include/cjson)
+find_library(cJSON_LIBRARY NAMES cjson HINTS /usr/lib)
 
-if (CJSON_INCLUDE_DIR AND CJSON_LIBRARY)
+if (cJSON_INCLUDE_DIR AND cJSON_LIBRARY)
     set(cJSON_FOUND TRUE)
     set(cJSON_INCLUDE_DIRS ${CJSON_INCLUDE_DIR})
     set(cJSON_LIBRARIES ${CJSON_LIBRARY})
@@ -14,4 +12,4 @@ else()
     set(cJSON_FOUND FALSE)
 endif()
 
-mark_as_advanced(CJSON_INCLUDE_DIR CJSON_LIBRARY)
+mark_as_advanced(cJSON_INCLUDE_DIR cJSON_LIBRARY)
