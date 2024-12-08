618cde21f57b59a99d6dc0a006fb744c3a84cc07
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-12-08 09:24:24 +0100

    Update casing

--- cmake/FindcJSON.cmake
+++ cmake/FindcJSON.cmake
@@ -1,13 +1,13 @@
 # FindcJSON.cmake
 # Locate cJSON library and include directories
 
-find_path(cJSON_INCLUDE_DIR NAMES cJSON.h)
-find_library(cJSON_LIBRARY NAMES cjson)
+find_pathCJSON_INCLUDE_DIRS NAMES cJSON.h)
+find_library(CJSON_LIBRARY NAMES cjson)
 
-if (cJSON_INCLUDE_DIR AND cJSON_LIBRARY)
+if (CJSON_INCLUDE_DIR AND CJSON_LIBRARY)
     set(cJSON_FOUND TRUE)
-    set(cJSON_INCLUDE_DIRS ${cJSON_INCLUDE_DIR})
-    set(cJSON_LIBRARIES ${cJSON_LIBRARY})
+    set(cJSON_INCLUDE_DIRS ${CJSON_INCLUDE_DIR})
+    set(cJSON_LIBRARIES ${CJSON_LIBRARY})
 else()
     set(cJSON_FOUND FALSE)
 endif()
