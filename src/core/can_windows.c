04c5ee9aa20a705cf247f086287041f9ef16c628
Michael Fitzmayer <mail@michael-fitzmayer.de>
2024-12-11 13:44:04 +0100

    Fix resource leak, detach thread

--- src/core/can_windows.c
+++ src/core/can_windows.c
@@ -220,6 +220,7 @@ void can_quit(core_t* core)
     }
 
     os_detach_thread(core->can_monitor_th);
+    core->can_monitor_th = NULL;
 }
 
 uint32 can_write(can_message_t* message, disp_mode_t disp_mode, const char* comment)
--- src/core/codb.c
+++ src/core/codb.c
@@ -99,6 +99,12 @@ void codb_deinit(void)
         ds301 = NULL;
     }
 
+    if (init_th != NULL)
+    {
+        os_detach_thread(init_th);
+        init_th = NULL;
+    }
+
     unload_codb();
 }
 
