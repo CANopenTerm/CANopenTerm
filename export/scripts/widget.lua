4c8fe437a165db46639221eddf286fcc8b21589e
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-03-28 11:08:48 +0100

    Update widget demo.

--- export/scripts/widget.lua
+++ export/scripts/widget.lua
@@ -8,6 +8,8 @@ License: Public domain
 show_window()
 
 previous_data = 0
+value = 0
+dir = 0
 
 while not key_is_hit() do
     id, length, data = can_read()
@@ -20,8 +22,21 @@ while not key_is_hit() do
         end
     end
 
+    if value >= 5000 then
+        dir = 1
+    elseif value <= 0 then
+        dir = 0
+    end
+
+    if dir == 0 then
+        value = value + 10
+    else
+        value = value - 10
+    end
+
     clear_window()
     widget_tachometer(10, 10, 100, 5000, data)
+    widget_tachometer(120, 10, 100, 5000, value)
     update_window()
 end
 
