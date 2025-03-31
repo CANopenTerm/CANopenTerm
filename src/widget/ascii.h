8340371e91e80cda646af2503929956be12775df
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-03-31 21:21:20 +0200

    Add ASCII widget font.

---
+++ src/widget/ascii.h
@@ -0,0 +1,13 @@
+/** @file ascii.h
+ *
+ *  A versatile software tool to analyse and configure CANopen devices.
+ *
+ *  Copyright (c) 2025, Michael Fitzmayer. All rights reserved.
+ *  SPDX-License-Identifier: MIT
+ *
+ **/
+
+#ifndef ASCII_H
+#define ASCII_H
+
+#endif /* ASCII_H */
