4e091f9325bcdd8e63b4801139d6bde8f1ec0bf5
Michael Fitzmayer <mail@michael-fitzmayer.de>
2026-03-28 17:04:39 +0100

    Update FindCANvenient.cmake

--- cmake/FindCANvenient.cmake
+++ cmake/FindCANvenient.cmake
@@ -1,17 +1,33 @@
 cmake_minimum_required(VERSION 3.16)
 
 # FindCANvenient.cmake
-# Locate CANvenient library and include directories
 
-find_path(CANvenient_INCLUDE_DIR NAMES CANvenient.h HINTS /usr/include)
-find_library(CANvenient_LIBRARY NAMES CANvenient HINTS /usr/lib)
+find_path(CANvenient_INCLUDE_DIR 
+  NAMES CANvenient.h
+  PATH_SUFFIXES include
+)
 
-if (CANvenient_INCLUDE_DIR AND CANvenient_LIBRARY)
-  set(CANvenient_FOUND TRUE)
+find_library(CANvenient_LIBRARY 
+  NAMES CANvenient
+  PATH_SUFFIXES lib lib64
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CANvenient
+  REQUIRED_VARS CANvenient_LIBRARY CANvenient_INCLUDE_DIR
+)
+
+if(CANvenient_FOUND)
   set(CANvenient_INCLUDE_DIRS ${CANvenient_INCLUDE_DIR})
   set(CANvenient_LIBRARIES ${CANvenient_LIBRARY})
-else()
-  set(CANvenient_FOUND FALSE)
+
+  if(NOT TARGET CANvenient::CANvenient)
+    add_library(CANvenient::CANvenient UNKNOWN IMPORTED)
+    set_target_properties(CANvenient::CANvenient PROPERTIES
+      IMPORTED_LOCATION "${CANvenient_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${CANvenient_INCLUDE_DIR}"
+    )
+  endif()
 endif()
 
 mark_as_advanced(CANvenient_INCLUDE_DIR CANvenient_LIBRARY)
