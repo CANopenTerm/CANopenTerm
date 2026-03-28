d120d85ae50742f1c55c26372a533c89f89e7460
Michael Fitzmayer <mail@michael-fitzmayer.de>
2026-03-28 19:42:06 +0100

    Attempt to fix Yocto build.

--- cmake/FindCANvenient.cmake
+++ cmake/FindCANvenient.cmake
@@ -4,14 +4,14 @@ cmake_minimum_required(VERSION 3.16)
 # Locate CANvenient library and include directories
 
 find_path(CANvenient_INCLUDE_DIR NAMES CANvenient.h PATHS /usr/include)
-find_library(CANvenient_LIBRARY NAMES libCANvenient.so PATHS /usr/lib)
+find_library(CANvenient_LIBRARY NAMES CANvenient PATHS /usr/lib)
 
 message(STATUS "CANvenient_INCLUDE_DIR: ${CANvenient_INCLUDE_DIR}")
 message(STATUS "CANvenient_LIBRARY: ${CANvenient_LIBRARY}")
 
 if (CANvenient_INCLUDE_DIR AND CANvenient_LIBRARY)
   set(CANvenient_FOUND TRUE)
-  set(CANvenient_INCLUDE_DIRS ${CANvenient_INCLUDE_DIR}/CANvenient)
+  set(CANvenient_INCLUDE_DIRS ${CANvenient_INCLUDE_DIR})
   set(CANvenient_LIBRARIES ${CANvenient_LIBRARY})
 else()
   set(CANvenient_FOUND FALSE)
