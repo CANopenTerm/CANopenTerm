1147e1fd16a620ce98021582e33810969d82e157
Michael Fitzmayer <mail@michael-fitzmayer.de>
2026-08-06 13:53:59 +0200

    Update SDL3 to most recent release version 3.4.14

--- cmake/os_windows.cmake
+++ cmake/os_windows.cmake
@@ -199,7 +199,7 @@ ExternalProject_Add(Lua_devel
 )
 
 # SDL3
-set(SDL3_VERSION "3.4.12")
+set(SDL3_VERSION "3.4.14")
 
 set(SDL3_DEVEL_PKG SDL3-devel-${SDL3_VERSION}-VC.zip)
 set(SDL3_PLATFORM  "x64")
@@ -212,7 +212,7 @@ set(SDL3_PATH ${CMAKE_CURRENT_SOURCE_DIR}/deps_${PLATFORM}/SDL3-${SDL3_VERSION}_
 
 ExternalProject_Add(SDL3_devel
   URL https://github.com/libsdl-org/SDL/releases/download/release-${SDL3_VERSION}/${SDL3_DEVEL_PKG}
-  URL_HASH SHA1=9b065c984f5c3cdf7ca1cfbd028ca34ea9f38555
+  URL_HASH SHA1=3b4b537202060f087bd8912f0dc7c9f497ca13ee
   DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps_${PLATFORM}
   DOWNLOAD_NO_PROGRESS true
   DOWNLOAD_EXTRACT_TIMESTAMP true
